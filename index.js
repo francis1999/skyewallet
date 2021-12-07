6d2bf8f6fc01d57f374b1b68616a0040fd033a4d
francis1999 <gbade.francis@gmail.com>
2021-12-07 11:45:54 +0000

    index.js created successfull which comprises of database connections, server connection, adding of dependencies, adding of env files, routers connector and request routes

---
+++ index.js
@@ -0,0 +1,51 @@
+//Adding of Dependencies
+const express = require("express")
+const app = express();
+const helmet = require("helmet");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+//This is the Router Connect
+
+
+
+//middleware
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan("common"));
+
+
+
+//MongoDB Database Connection goes here
+mongoose.connect(process.env.DATABASEURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Database Connected Successfuly")
+}).catch(() => {
+    console.log("Opps!!! Error in Connection");
+})
+
+//This is routes for all my requests
+
+
+
+app.get("/", (req, res) => {
+    res.send("Skye Wallet Application");
+})
+
+
+
+
+
+app.use('/uploads', express.static('uploads'))
+const port = process.env.PORT;
+
+app.listen(port, () => {
+    console.log(`We are running on port ${port}`);
+})
